307abf806d52e27c7e7c498b3dcf9793312c3478
Sairam <pvsairam@hotmail.com>
2024-07-28 15:31:53 +0530

    Update PERC20Sample.sol

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("Happy Cuan Xaviera", "HCX") {}
+    constructor() PERC20("AirdropOG", "AOG") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
